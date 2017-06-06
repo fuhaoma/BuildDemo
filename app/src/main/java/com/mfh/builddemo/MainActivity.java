b2fdcdf6d5408ee812450aa149f7f45fd2443e58
马富豪 <ma_fuhao@126.com>
2017-06-06 16:36:08 +0800

    #添加#多渠道打包

--- app/src/main/java/com/mfh/builddemo/MainActivity.java
+++ app/src/main/java/com/mfh/builddemo/MainActivity.java
@@ -3,11 +3,25 @@ package com.mfh.builddemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.umeng.analytics.MobclickAgent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(MainActivity.this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(MainActivity.this);
     }
 }
